6002611159c6ef2cfddcff2d7a256a20a366a1d6
FahdMahrez <azabdull010@gmail.com>
2025-10-10 11:36:26 -0700

    Added User Repository

--- EShop/Context/ApplicationDbContext.cs
+++ EShop/Context/ApplicationDbContext.cs
@@ -11,5 +11,6 @@ namespace EShop.Context
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ EShop/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using EShop.Context;
+using EShop.Data;
+using EShop.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Repositories
+{
+    public class UserRepository(ApplicationDbContext dbContext) : IUserRepository
+    {
+        public async Task<bool> CreateAsync(User user, CancellationToken cancellationToken)
+         {  
+            await dbContext.Users.AddAsync(user, cancellationToken);
+            return await dbContext.SaveChangesAsync() > 0 ? true : false;
+         }    
+
+        public async Task<bool> DeleteAsync(User user, CancellationToken cancellationToken)
+        {
+            dbContext.Users.Remove(user);
+            return await dbContext.SaveChangesAsync(cancellationToken) > 0;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await dbContext.Users
+                            .AsNoTracking()
+                            .ToListAsync(cancellationToken);
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await dbContext.Users
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+        }
+
+        public async Task<bool> UpdateAsync(User user, CancellationToken cancellationToken)
+        {
+            dbContext.Users.Update(user);
+            return await dbContext.SaveChangesAsync(cancellationToken) > 0;
+        }
+    }
+}
